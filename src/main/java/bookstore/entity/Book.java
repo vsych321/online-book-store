57bf5607bcd5d30fcd37dd587d6e57abaa7f75ae
hp <volodya.gata@gmail.com>
2023-10-29 15:12:35 +0200

    fixed mistakes

--- src/main/java/bookstore/entity/Book.java
+++ src/main/java/bookstore/entity/Book.java
@@ -15,7 +15,6 @@ import org.hibernate.annotations.Where;
 
 @Getter
 @Setter
-@RequiredArgsConstructor
 @Entity
 @SQLDelete(sql = "UPDATE books SET is_deleted = true WHERE id =?")
 @Where(clause = "is_deleted=false")
