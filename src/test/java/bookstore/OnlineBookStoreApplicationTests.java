2b3fb9872e5f953fae80b8b1027b168ced9090a3
vsych321 <volodya.gata@gmail.com>
2024-02-12 20:07:30 +0200

    added tests for Book and Category entities

--- src/test/java/bookstore/OnlineBookStoreApplicationTests.java
+++ src/test/java/bookstore/OnlineBookStoreApplicationTests.java
@@ -1,11 +1,7 @@
 package bookstore;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class OnlineBookStoreApplicationTests {
-    @Test
-    void contextLoads() {
-    }
 }
